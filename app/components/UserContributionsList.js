1c75e62e31b93b9e50d32bbb0b0325e26fb7a752
Marc Farra <marcfarra@gmail.com>
2019-06-19 12:09:08 -0400

    Add no contributions screen

--- app/components/UserContributionsList.js
+++ app/components/UserContributionsList.js
@@ -1,13 +1,38 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import Container from '../components/Container'
 
 import UserContributionsItem from './UserContributionsItem'
+
+const TextContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+const Text = styled.Text`
+  font-size: 16;
+  letter-spacing: 0.5;
+`
 
 const StyledFlatList = styled.FlatList``
 
 export default class UserContributionsList extends React.Component {
   render () {
     const { data, onSelectItem } = this.props
+
+    if (data && !data.length) {
+      console.log('IM HERE')
+      return (
+        <Container>
+          <TextContainer>
+            <Text>No contributions to display.</Text>
+            <Text>Make an edit to have it appear here!</Text>
+          </TextContainer>
+        </Container>
+      )
+    }
+
     return (
       <StyledFlatList
         data={data}
