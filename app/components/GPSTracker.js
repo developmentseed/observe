82b8a9a32ab509d32f9f70dedb5c78f6f940f217
Sanjay B <sanjaybhangar@gmail.com>
2019-10-08 17:26:55 +0530

    lintfix

--- app/components/GPSTracker.js
+++ app/components/GPSTracker.js
@@ -4,8 +4,6 @@ import * as Permissions from 'expo-permissions'
 import * as Location from 'expo-location'
 
 class GPSTracker extends Component {
-
-
   componentDidMount () {
     this.startWatching()
   }
