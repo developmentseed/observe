7cc1dc4545aee38c8559d9e9293269497fcf5367
Sanjay B <sanjaybhangar@gmail.com>
2019-10-11 15:23:43 +0530

    [wip] add reducers

--- app/reducers/traces.js
+++ app/reducers/traces.js
@@ -1,10 +1,12 @@
 import * as types from '../actions/actionTypes'
 import {
-  getNewTrace
+  getNewTrace,
+  getPoint
 } from '../utils/traces'
 
 const initialState = {
   currentTrace: null,
+  watcher: null,
   traces: []
 }
 
@@ -16,6 +18,34 @@ export default function (state = initialState, action) {
         ...state,
         currentTrace: newTrace
       } 
+    }
+    case types.TRACE_POINT_CAPTURED: {
+      const point = getPoint(action.data)
+      return {
+        ...state,
+        currentTrace: {
+          points: [...state.currentTrace.points, point]
+        }
+      }
+    }
+    case types.ENDED_TRACE: {
+      const newTrace = {
+        points: currentTrace.points,
+        id: 'foo', // FIXME: generate random id
+        pending: true
+      }
+      return {
+        ...state,
+        currentTrace: null,
+        watcher: null,
+        traces: [...state.traces, newTrace]
+      }
+    }
+    case types.SET_TRACE_SUBSCRIPTION: {
+      return {
+        ...state,
+        watcher: action.data
+      }
     }
   }
 }
