c6771e424f691d6f133096e3a1a3f318df8d4f87
Sanjay B <sanjaybhangar@gmail.com>
2019-10-14 19:36:27 +0530

    add trace to traceJSON util

--- app/actions/traces.js
+++ app/actions/traces.js
@@ -1,9 +1,7 @@
 import traceService from '../services/trace'
 
 export function startTrace () {
-  console.log('called start trace')
   return (dispatch, getState) => {
-    console.log('dispatch start trace')
     const currentTrace = getState().traces.currentTrace
     if (currentTrace) {
       console.error('startTrace called with trace already running')
@@ -14,9 +12,7 @@ export function startTrace () {
 }
 
 export function endTrace () {
-  console.log('called end trace')
   return (dispatch, getState) => {
-    console.log('dispatched end trace')
     const { watcher, currentTrace } = getState().traces
     if (!currentTrace) {
       console.error('endTrace called with no current trace')
--- app/utils/traces.js
+++ app/utils/traces.js
@@ -8,3 +8,51 @@ export function getNewTrace () {
 export function getPoint (location) {
   return { ...location.coords }
 }
+
+/**
+ *
+ * @param {Object} trace - trace object
+ * @returns {Object} - TraceJSON representation of trace
+ * @example
+ * const trace = {
+ *   'points': [
+ *     {
+ *       'latitude': 1.0,
+ *       'longitude': 1.0,
+ *       'timestamp': 100
+ *     },
+ *     {
+ *       'latitude': 2.0,
+ *       'longitude': 2.0,
+ *       'timestamp': 200
+ *     }
+ *   ]
+ * }
+ * toTraceJSON(trace)
+ * {
+ *   type: 'Feature',
+ *   properties: {
+ *     timestamps: [
+ *       100, 200
+ *     ]
+ *   },
+ *   coordinates: [
+ *     [1.0,1.0],
+ *     [2.0,2.0]
+ *   ]
+ * }
+ */
+export function toTraceJSON (trace) {
+  const timestamps = trace.points.map(t => t.timestamp)
+  const coordinates = trace.points.map(t => [t.longitude, t.latitude])
+  return {
+    type: 'Feature',
+    properties: {
+      timestamps
+    },
+    geometry: {
+      type: 'LineString',
+      coordinates
+    }
+  }
+}
