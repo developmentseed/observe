2293b2f1a6125125c8ef80d38e12deb8a64862d0
Sanjay B <sanjaybhangar@gmail.com>
2019-10-17 17:11:43 +0530

    fix tests

--- __tests__/actions/trace.test.js
+++ __tests__/actions/trace.test.js
@@ -4,7 +4,9 @@ import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import {
   startTrace,
-  endTrace
+  endTrace,
+  pauseTrace,
+  unpauseTrace
 } from '../../app/actions/traces'
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
@@ -32,13 +34,29 @@ jest.mock('../../app/services/trace', () => {
         })
       }
     },
-    endTrace: (dispatch, watcher) => {
+    endTrace: (dispatch, watcher, description) => {
       dispatch({
-        type: 'ENDED_TRACE'
+        type: 'ENDED_TRACE',
+        description
       })
     }
   }
 })
+
+const getMockCurrentTrace = function () {
+  return {
+    points: [{
+      longitude: 1.0,
+      latitude: 2.0,
+      timestamp: 100
+    }, {
+      longitude: 2.0,
+      latitude: 3.0,
+      timestamp: 200
+    }],
+    paused: false
+  }
+}
 
 describe('test trace actions', () => {
   it('should start trace correctly', () => {
@@ -55,14 +73,33 @@ describe('test trace actions', () => {
   it('should end trace correctly', () => {
     const store = mockStore({
       traces: {
-        currentTrace: {
-          points: [[0, 0], [1, 1]]
-        }
+        currentTrace: getMockCurrentTrace()
       }
     })
-    store.dispatch(endTrace())
+    store.dispatch(endTrace('test description'))
     const actions = store.getActions()
     expect(actions.length).toEqual(1)
     expect(actions[0].type).toEqual('ENDED_TRACE')
+    expect(actions[0].description).toEqual('test description')
+  })
+  it('should pause trace', () => {
+    const store = mockStore({
+      traces: {
+        currentTrace: getMockCurrentTrace()
+      }
+    })
+    store.dispatch(pauseTrace())
+    const actions = store.getActions()
+    expect(actions[0].type).toEqual('PAUSED_TRACE')
+  })
+  it('should unpause trace', () => {
+    const store = mockStore({
+      traces: {
+        currentTrace: getMockCurrentTrace()
+      }
+    })
+    store.dispatch(unpauseTrace())
+    const actions = store.getActions()
+    expect(actions[0].type).toEqual('UNPAUSED_TRACE')
   })
 })
