cd364b0343a0527ddb7219882f43aeda2ef6fada
Sanjay B <sanjaybhangar@gmail.com>
2019-10-18 14:18:52 +0530

    add actions for startSavingTrace and discardTrace

--- app/actions/traces.js
+++ app/actions/traces.js
@@ -34,3 +34,24 @@ export function endTrace (description = '') {
     traceService.endTrace(dispatch, watcher, description)
   }
 }
+
+/**
+ * Used to set a `saving` boolean on traces.
+ * We need this to know to hide the RecordHeader on the saveTraces screen
+ * // FIXME: ideally we would not need this as a separate action
+ */
+export function startSavingTrace () {
+  return {
+    type: types.TRACE_START_SAVING
+  }
+}
+
+export function discardTrace () {
+  return (dispatch, getState) => {
+    const { watcher } = getState().traces
+    if (watcher) watcher.remove()
+    dispatch({
+      type: types.TRACE_DISCARD
+    })
+  }
+}
