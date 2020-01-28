82b04629131ba49025902a73c34a5fec28c8a578
sethvincent <sethvincent@gmail.com>
2020-01-28 13:49:17 -0800

    keep android version code the same as ios build version

--- version-ios.sh
+++ version-ios.sh
@@ -4,6 +4,7 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 
 cd ios
 xcrun agvtool new-marketing-version $PACKAGE_VERSION
-xcrun agvtool next-version
-git add .
+BUILD_VERSION=$(xcrun agvtool next-version)
+echo 'VERSION_CODE=$BUILD_VERSION' > ../android/version.properties
+# git add .
 cd ..
