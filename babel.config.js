6c0101edfda790779492ca0b3aee9d883ee6a252
Sanjay B <sanjaybhangar@gmail.com>
2020-02-14 16:41:29 +0530

    apply console-remove babel rule only in production

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['transform-remove-console']
+  env: {
+    production: {
+      plugins: ['transform-remove-console']
+    }
+  }
 }
