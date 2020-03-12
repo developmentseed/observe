46b05aa40921567faac21abb297e94c6e9658cc2
sethvincent <sethvincent@gmail.com>
2020-03-11 18:10:40 -0700

    add CompleteWayButton and onCompleteWayPress function prop

--- app/components/WayEditingOverlay.js
+++ app/components/WayEditingOverlay.js
@@ -42,6 +42,22 @@ const ActionButton = styled(AnimatedTouchable)`
   align-items: center;
 `
 
+const CompleteWayButton = styled.TouchableHighlight`
+  border-radius: 100;
+  width: 56;
+  height: 56;
+  background-color: ${colors.primary};
+  elevation: 2;
+  shadow-color: ${colors.base};
+  shadow-opacity: 0.7;
+  shadow-offset: 0px 0px;
+  bottom: 100;
+  right: 16;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+`
+
 class WayEditingOverlay extends React.Component {
   render () {
     const {
@@ -49,27 +65,33 @@ class WayEditingOverlay extends React.Component {
       onUndoPress,
       onAddNodePress,
       onRedoPress,
-      onMoveNodePress
+      onMoveNodePress,
+      onCompleteWayPress
     } = this.props
 
     return (
-      <MenuWrapper>
-        <ActionButton onPress={onDeleteNodePress}>
-          <Icon name='trash-bin' size={20} color='#6579FC' />
-        </ActionButton>
-        <ActionButton onPress={onUndoPress}>
-          <Icon name='arrow-semi-spin-ccw' size={20} color='#6579FC' />
-        </ActionButton>
-        <AddNodeButton onPress={onAddNodePress}>
-          <Icon name='plus' size={20} color='#6579FC' />
-        </AddNodeButton>
-        <ActionButton onPress={onRedoPress}>
-          <Icon name='arrow-semi-spin-cw' size={20} color='#6579FC' />
-        </ActionButton>
-        <ActionButton onPress={onMoveNodePress}>
-          <Icon name='arrow-move' size={20} color='#6579FC' />
-        </ActionButton>
-      </MenuWrapper>
+      <>
+        <CompleteWayButton onPress={onCompleteWayPress}>
+          <Icon name='tick' size={20} color='#FFFFFF' />
+        </CompleteWayButton>
+        <MenuWrapper>
+          <ActionButton onPress={onDeleteNodePress}>
+            <Icon name='trash-bin' size={20} color='#6579FC' />
+          </ActionButton>
+          <ActionButton onPress={onUndoPress}>
+            <Icon name='arrow-semi-spin-ccw' size={20} color='#6579FC' />
+          </ActionButton>
+          <AddNodeButton onPress={onAddNodePress}>
+            <Icon name='plus' size={20} color='#6579FC' />
+          </AddNodeButton>
+          <ActionButton onPress={onRedoPress}>
+            <Icon name='arrow-semi-spin-cw' size={20} color='#6579FC' />
+          </ActionButton>
+          <ActionButton onPress={onMoveNodePress}>
+            <Icon name='arrow-move' size={20} color='#6579FC' />
+          </ActionButton>
+        </MenuWrapper>
+      </>
     )
   }
 }
