a5eba7e333fa480bef82ff9672c99c4d69fde968
Sajjad Anwar <sajjadkm@gmail.com>
2020-03-20 19:53:16 +0530

    remove the asset folders before creating new ones so the broken links go away. add link to the update icons script

--- scripts/link-icons.sh
+++ scripts/link-icons.sh
@@ -2,11 +2,26 @@
 
 ASSETS_DIR=${PWD}/app/assets
 IOS_DIR=${PWD}/ios/Observe/Images.xcassets
+IOS_TEMP=${PWD}/ios/Observe/Images.xcassets-temp
 ANDROID_DIR=${PWD}/android/app/src/main/res/drawable-xxhdpi
 
 ICON_DIRS="${ASSETS_DIR}/fontawesome/*.png
 ${ASSETS_DIR}/maki/*.png
 ${ASSETS_DIR}/temaki/*.png"
+
+# move the existing iOS imageset to a temp folder
+mv $IOS_DIR $IOS_TEMP
+
+# create a new ios assets dir
+mkdir $IOS_DIR
+
+# copy over app icon and metadata
+cp -r $IOS_TEMP/AppIcon.appiconset $IOS_DIR
+cp $IOS_TEMP/Contents.json $IOS_DIR
+
+# remove all existing links from android
+rm -rf $ANDROID_DIR
+mkdir $ANDROID_DIR
 
 for file in $ICON_DIRS
 do
@@ -25,3 +40,6 @@ do
   node ./scripts/create-ios-icon-contents.js $CONTENTS_FILEPATH $FILENAME
   ln -s $file $IMAGESET_PATH
 done
+
+# remove temp
+rm -rf $IOS_TEMP
