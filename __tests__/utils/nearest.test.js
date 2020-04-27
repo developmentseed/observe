96cf8a9cb8c3e83ec49c7856c5a94fab89560806
Sajjad Anwar <sajjadkm@gmail.com>
2020-04-27 16:36:29 +0530

    find nearest nodes on the edges

--- __tests__/utils/nearest.test.js
+++ __tests__/utils/nearest.test.js
@@ -5,32 +5,41 @@
 import fs from 'fs'
 import path from 'path'
 import { findNearest } from '../../app/utils/nearest'
+import { addNodes } from '../../app/services/nodecache'
 
-test('find nearest features for a node', () => {
-  const features = JSON.parse(fs.readFileSync(path.join(__dirname, '../fixtures/osm-geojson.geojson'), { 'encoding': 'utf-8' }))
+test('find nearest features for a node', async () => {
+  const features = JSON.parse(fs.readFileSync(path.join(__dirname, '../fixtures/osm-geojson-dc.geojson'), { 'encoding': 'utf-8' }))
 
-  // node close to a way / linestring
+  const nodes = JSON.parse(fs.readFileSync(path.join(__dirname, '../fixtures/dc-nodes.json'), { 'encoding': 'utf-8' }))
+  const tile = '0320100322313212'
+  await addNodes(tile, nodes)
+
+  // node close to a polygon
   const node1 = {
     'type': 'Feature',
     'geometry': {
       'type': 'Point',
-      'coordinates': [77.6109, 12.9764]
+      'coordinates': [-77.0244, 38.9023]
     },
     'properties': {}
   }
 
-  // node close to a way / polygon
+  // node close to a linestring
   const node2 = {
     'type': 'Feature',
     'geometry': {
       'type': 'Point',
-      'coordinates': [77.6029, 12.9681]
+      'coordinates': [-77.0245, 38.9025]
     },
     'properties': {}
   }
 
-  const nearest1 = findNearest(node1, features)
-  const nearest2 = findNearest(node2, features)
+  const nearest1 = await findNearest(node1, features)
+  const nearest2 = await findNearest(node2, features)
+  // const featureCollection = {
+  //   'type': 'FeatureCollection',
+  //   'features': nearest2.nearestEdges.concat(nearest2.nearestNodes)
+  // }
   expect(nearest1).toMatchSnapshot()
   expect(nearest2).toMatchSnapshot()
 })
