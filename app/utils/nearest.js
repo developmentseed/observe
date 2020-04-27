c5fb4c6e3a8c672aba13af8920da6097d55db4d1
Sajjad Anwar <sajjadkm@gmail.com>
2020-04-27 16:36:29 +0530

    find nearest nodes on the edges

--- app/utils/nearest.js
+++ app/utils/nearest.js
@@ -4,38 +4,58 @@
 import turfPointToLineDistance from '@turf/point-to-line-distance'
 import turfDistance from '@turf/distance'
 import { lineString } from '@turf/helpers'
+import { nodesGeojson } from '../utils/nodes-to-geojson'
 
-export function findNearest (node, features) {
-  const threshold = 0.02
+// FIXME: adjust these based on interactions
+const threshold = 0.01
+const nodeThreshold = 0.02
+
+export async function findNearest (node, features) {
   const nearestEdges = []
-  const nearestNodes = []
-  features.features.forEach(feature => {
-    if (feature.geometry.type === 'LineString') {
-      const distance = turfPointToLineDistance(node, feature, { 'units': 'kilometers' })
-      if (distance < threshold) {
-        feature.properties['distance'] = distance
-        nearestEdges.push(feature)
-      }
-    }
+  let nearestNodes = []
+  for (const index in features.features) {
+    const feature = features.features[index]
 
     if (feature.geometry.type === 'Point') {
       const distance = turfDistance(node, feature, { 'units': 'kilometers' })
       if (distance < threshold) {
+        feature.properties.distance = distance
         nearestNodes.push(feature)
+      }
+    }
+
+    if (feature.geometry.type === 'LineString') {
+      const nodes = await getNodes(feature)
+      const distance = turfPointToLineDistance(node, feature, { 'units': 'kilometers' })
+      if (distance < threshold) {
+        feature.properties['distance'] = distance
+        nearestEdges.push(feature)
+        // check if there are nearby member nodes
+        const nearbyNodes = getNearbyMemberNodes(node, nodes)
+        if (nearbyNodes.length) {
+          nearestNodes = nearestNodes.concat(nearbyNodes)
+        }
       }
     }
 
     if (feature.geometry.type === 'Polygon') {
       // get all the edges
       const edges = getEdge(feature)
+      const nodes = await getNodes(feature)
       edges.forEach(edge => {
         const distance = turfPointToLineDistance(node, edge, { 'units': 'kilometers' })
         if (distance < threshold) {
+          edge.properties.distance = distance
           nearestEdges.push(edge)
+          const nearbyNodes = getNearbyMemberNodes(node, nodes)
+          if (nearbyNodes.length) {
+            nearestNodes = nearestNodes.concat(nearbyNodes)
+          }
         }
       })
     }
-  })
+  }
+
   return {
     nearestEdges,
     nearestNodes
@@ -51,4 +71,24 @@ function getEdge (polygon) {
     edges.push(edgeLine)
   }
   return edges
+}
+
+async function getNodes (feature) {
+  // get all nodes of this way
+  const nodeIds = feature.properties.ndrefs.map(n => {
+    return `node/${n}`
+  })
+  const geojson = await nodesGeojson(nodeIds)
+  return geojson
+}
+
+function getNearbyMemberNodes (point, nodes) {
+  const nearbyNodes = []
+  nodes.features.forEach(node => {
+    const distance = turfDistance(point, node)
+    if (distance < nodeThreshold) {
+      nearbyNodes.push(node)
+    }
+  })
+  return nearbyNodes
 }
