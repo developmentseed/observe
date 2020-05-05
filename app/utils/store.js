7ca990a189b7779effad426545dced968c6758c7
Vitor George <vitor.george@gmail.com>
2020-05-05 20:58:47 +0100

    Enable Redux Dev Tools

--- app/utils/store.js
+++ app/utils/store.js
@@ -1,19 +1,20 @@
 import { createNetworkMiddleware } from 'react-native-offline'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore } from 'redux-persist'
 import thunk from 'redux-thunk'
 
 import rootReducer from '../reducers'
 
-// Add initial state here
 const initialState = {}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const networkMiddleware = createNetworkMiddleware()
 
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(networkMiddleware, thunk)
+  composeEnhancers(applyMiddleware(networkMiddleware, thunk))
 )
 
 let persistor = persistStore(store)
