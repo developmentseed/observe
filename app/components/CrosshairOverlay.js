3219ed86a51c894eec54b1f0bb12ad86c28e4ad7
sethvincent <sethvincent@gmail.com>
2020-05-29 17:23:31 -0700

    fix crosshair position while tracing gpx and editing features, remove unused component

--- app/components/CrosshairOverlay.js
+++ app/components/CrosshairOverlay.js
@@ -4,11 +4,13 @@ import styled from 'styled-components/native'
 import Icon from './Collecticons'
 import { colors } from '../style/variables'
 
+const marginTop = Platform.OS === 'ios' ? 49 : 17
+
 const Container = styled.View`
   position: absolute;
   left: 50%;
   top: 50%;
-  margin-top: ${Platform.OS === 'ios' ? 49 : 17};
+  margin-top: ${marginTop};
   margin-left: -15;
 `
 
@@ -16,8 +18,12 @@ const Crosshair = styled.Text``
 
 export default class CrosshairOverlay extends Component {
   render () {
+    const style = {
+      marginTop: this.props.isRecording ? marginTop + 50 : marginTop
+    }
+
     return (
-      <Container>
+      <Container style={style}>
         <Crosshair>
           <Icon name='crosshair-2' size={30} color={colors.primary} />
         </Crosshair>
