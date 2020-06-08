99b2fc714952193c89795b6b3fc40d0c9d424587
Sajjad Anwar <sajjadkm@gmail.com>
2020-06-08 18:18:22 +0530

    handle way modify cases that delete the way when converting edits to geojson

--- app/utils/edits-to-geojson.js
+++ app/utils/edits-to-geojson.js
@@ -1,5 +1,6 @@
 import _find from 'lodash.find'
 import { addIconUrl } from '../utils/add-icon-url'
+import _cloneDeep from 'lodash.clonedeep'
 
 export default function editsToGeojson (edits) {
   const editsGeojson = {
@@ -11,19 +12,21 @@ export default function editsToGeojson (edits) {
       if (!_find(editsGeojson.features, { 'id': e.id })) {
         // set tag information for this feature
 
-        // Add feature to editsGeojson. If feature was deleted and is pending
-        // upload, add a property to flag this.
-        const feature =
-          e.type === 'delete'
-            ? {
-              ...e.oldFeature,
-              properties: {
-                ...e.oldFeature.properties,
-                pendingDeleteUpload: true
-              }
-            }
-            : e.newFeature
-
+        // Add feature to editsGeojson.
+        let feature
+        if (e.type === 'delete') {
+          // If feature was deleted and is pending
+          // upload, add a property to flag this.
+          feature = _cloneDeep(e.oldFeature)
+          feature.properties.pendingDeleteUpload = true
+        } else if (e.type === 'modify' && e.newFeature.geometry.type !== 'Point' && e.newFeature.geometry.coordinates.length < 2) {
+          // this is a special case when one of two nodes of a way is deleted, then the way itself is deleted.
+          // in the edits this is considered as a modify operation because it may involve other sharedways. Read for more https://github.com/developmentseed/observe/issues/296
+          feature = _cloneDeep(e.oldFeature)
+          feature.properties.pendingDeleteUpload = true
+        } else {
+          feature = e.newFeature
+        }
         // add this feature to the editsGeojson
         editsGeojson.features.push(addIconUrl(feature))
       }
